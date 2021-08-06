6640cd5dc9cff5bd5e76b137792f4b4b258b2222
Fedor Livshits <fediliv@gmail.com>
2021-08-07 02:54:53 +0300

    Добавлен компонент и стили для слайдера с товаром

---
+++ src/Components/SliderProducts/SliderProducts.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react'
+import {Container} from '../../GlobalStyle'
+import Carousel from 'react-elastic-carousel'
+import './customSlider.css'
+import {Link, SliderProductsWrapper, Title} from './SliderProductsStyle'
+import ProductCard from '../ProductCard/ProductCard'
+
+
+const breakPoints = [
+    {width: 1, itemsToShow: 1},
+    {width: 550, itemsToShow: 2, itemsToScroll: 2},
+    {width: 768, itemsToShow: 3},
+    {width: 1200, itemsToShow: 4}
+]
+
+interface ProductsType {
+    id: string | null
+    photo: string | null
+    'product-name': string | null
+    'product-designer': string | null
+    'about-product': string | null
+    price: string | null
+}
+
+const SliderProducts = () => {
+    const [products, setProducts] = useState<Array<ProductsType> | null>(null)
+
+    useEffect(() => {
+        fetch('data.json', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+            })
+            .catch(() => setProducts([]))
+    }, [])
+    return (
+        <>
+            <Container>
+                <SliderProductsWrapper>
+                    <Title>
+                        ИЗБРАННОЕ НАШИМ СООБЩЕСТВОМ
+                    </Title>
+                    <Link>
+                        Посмотреть все товары
+                    </Link>
+                    <Carousel breakPoints={breakPoints} isRTL={false}>
+                        {products?.map(item => {
+                            return <ProductCard key={item.id} img={item.photo} name={item['product-name']}
+                                                designer={item['product-designer']} price={item.price}/>
+                        })}
+                    </Carousel>
+                </SliderProductsWrapper>
+            </Container>
+        </>
+    )
+}
+
+export default SliderProducts
---
+++ src/Components/SliderProducts/SliderProductsStyle.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const SliderProductsWrapper = styled.div`
+   min-height: 100vh;
+`
+export const Title = styled.h3`
+   padding-top: 70px;
+   font-size: 30px;
+   line-height: 40px;
+   font-weight: 400;  
+`
+export const Link = styled.a`
+   display: inline-block;
+   margin: 20px 10px 40px 0;
+   font-size: 14px;
+   line-height: 22px;
+   font-weight: 400;
+   &:hover{
+    text-decoration: underline;
+   }
+`
